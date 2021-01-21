5adc7782a48f47c296df141defa2ebc499070e2b
hanaelaz <50340523+hanaelaz@users.noreply.github.com>
2021-01-21 15:34:47 +0100

    Update PersonController.java

--- src/main/java/com/demo/api/PersonController.java
+++ src/main/java/com/demo/api/PersonController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @GetMapping()
     public String home() {
-                return "Welcome this is home page, Please:"+"<br/><br/>"+"- Add <B>/hello/{yourname}</B> in the path <br/> - Add <B>/spring</B> to see definition of Spring";
+                return "Welcome this is home page, Please:"+"<br/><br/><br/>"+"- Add <B>/hello/{yourname}</B> in the path <br/><br/> - Add <B>/spring</B> to see definition of Spring";
     }
 
     @GetMapping("/spring")
     public String test() {
-        return "Hello World from Spring"+"<br/><br/> "+ "<B>Definition: </B>The Spring Framework is an application framework and inversion of control container for the Java platform. The framework's core features can be used by any Java application, but there are extensions for building web applications on top of the Java EE platform. ";
+        return "Hello World from <mark>Spring</mark>"+"<br/><br/> "+ "<B>Definition: </B>The Spring Framework is an application framework and inversion of control container for the Java platform. The framework's core features can be used by any Java application, but there are extensions for building web applications on top of the Java EE platform. ";
     }
     @GetMapping(path = "/hello/{name}")
     public String message(@PathVariable("name") String name) {
