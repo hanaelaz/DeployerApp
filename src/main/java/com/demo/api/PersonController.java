5c095f6c10ba7b603d2959cddc4ba414a114d286
hanaelaz <50340523+hanaelaz@users.noreply.github.com>
2021-01-21 15:04:26 +0100

    Update PersonController.java
    modifier fonction message

--- src/main/java/com/demo/api/PersonController.java
+++ src/main/java/com/demo/api/PersonController.java
@@ -13,16 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @GetMapping()
     public String home() {
-                return "this is home page please add /bonjour/yourname in the path";
+                return "this is home page please add /hello/{yourname} in the path";
     }
 
     @GetMapping("/spring")
     public String test() {
         return "Hello World from Spring"+" "+ "The Spring Framework is an application framework and inversion of control container for the Java platform. The framework's core features can be used by any Java application, but there are extensions for building web applications on top of the Java EE platform. ";
     }
-    @GetMapping(path = "/bonjour/{name}")
+    @GetMapping(path = "/hello/{name}")
     public String message(@PathVariable("name") String name) {
-        return "Bonjour : "+name;
+        return "Hello : "+name+" "+"Well you have successfully deployed your app";
     }
     
 }
