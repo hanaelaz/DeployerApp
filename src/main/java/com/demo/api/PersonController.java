5683b0537a326808da053487fd044da2af877629
hanaelaz <50340523+hanaelaz@users.noreply.github.com>
2021-01-21 15:43:29 +0100

    Update PersonController.java

--- src/main/java/com/demo/api/PersonController.java
+++ src/main/java/com/demo/api/PersonController.java
@@ -13,16 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @GetMapping()
     public String home() {
-                return "Welcome this is home page, Please:"+"<br/><br/><br/>"+"- Add <B>/hello/{yourname}</B> in the path <br/><br/> - Add <B>/spring</B> to see definition of Spring";
+                return "Welcome this is home page, Please:"+"<br/><br/><br/>"+"- Add <B>/hello/{yourname}</B> in the path to view your message. <br/><br/> - Add <B>/spring</B> to see definition of Spring.";
     }
 
     @GetMapping("/spring")
     public String test() {
-        return "Hello World from <mark>Spring</mark>"+"<br/><br/> "+ "<B>Definition: </B>The Spring Framework is an application framework and inversion of control container for the Java platform. The framework's core features can be used by any Java application, but there are extensions for building web applications on top of the Java EE platform. ";
+        return "<centre><strong>Hello World from <mark>Spring</mark></strong></center>"+"<br/><br/> "+ "<B>Definition: </B>The Spring Framework is an application framework and inversion of control container for the Java platform. The framework's core features can be used by any Java application, but there are extensions for building web applications on top of the Java EE platform. "
+            +"<br/><br/><br/><small> To return to the home page replace <B>/spring</B> with <B>/</B> in the path</small>";
     }
     @GetMapping(path = "/hello/{name}")
     public String message(@PathVariable("name") String name) {
-        return "Hello : <B>"+name+"</B> <br/><br/>"+"Congratulations, you have successfully deployed your app";
+        return "<center>Hello  <B>"+name+"</B> <br/><br/>"+"Congratulations, you have successfully deployed your app! ! ! </center>"+
+            "<br/><br/><br/><small> To return to the home page replace <B>/hello/{yourname}</B> with <B>/</B> in the path</small>";
     }
     
 }
